d0122f4eefd1edf6c87cfe53914ae1855770bcbc
Brian <bperin42@gmail.com>
2020-11-13 11:04:58 -0800

    more adapter stuff

--- app/src/main/java/com/brianperin/ddsample/adapter/RestaurantsAdapter.kt
+++ app/src/main/java/com/brianperin/ddsample/adapter/RestaurantsAdapter.kt
@@ -6,27 +6,36 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.brianperin.ddsample.R
-import kotlinx.android.synthetic.main.fragment_restaurants.view.*
+import com.brianperin.ddsample.network.response.RestaurantNetwork
 
 
-class RestaurantsAdapter() : RecyclerView.Adapter<RestaurantsAdapter.ViewHolder>() {
+/**
+ * Our main adapter for showing a list of restaurants
+ * Currently doesn't support adding / subtracting elements in an elegant way or endless scrolling
+ */
+class RestaurantsAdapter(restaurants: List<RestaurantNetwork>) : RecyclerView.Adapter<RestaurantsAdapter.ViewHolder>() {
+
+    private var restaurants = listOf<RestaurantNetwork>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val contactView =  LayoutInflater.from(parent.context).inflate(R.layout.recycler_item_restaurant, parent, false)
-
+        val restaurantView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_item_restaurant, parent, false)
+        return ViewHolder(restaurantView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
     }
 
-//    override fun getItemCount(): Int {
-//        TODO("Not yet implemented")
-//    }
+    override fun getItemCount(): Int {
+        return restaurants.size
+    }
 
     inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
 
-        val nameTextView = itemView.findViewById<TextView>(R.id.contact_name)
-        val messageButton = itemView.findViewById<TextView>(R.id.message_button)
+        val name: TextView = itemView.findViewById<TextView>(R.id.tv_restaurat_name)
+        val description: TextView = itemView.findViewById<TextView>(R.id.tv_restaurat_name)
+
+
     }
 }
